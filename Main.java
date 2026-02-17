e4459d9f7d1d39dac17fed53221ef6cd0ef93040
Saathvik Kamma <saathvikkamma@gmail.com>
2026-02-17 09:23:51 -0600

    working on the thig

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+import src.dice.DiceEnum;
 public class Main {
-    
+    public static void main(String[] args) {
+        for (DiceEnum dice : DiceEnum.values()) {
+            System.out.println(dice);
+        }
+    }
 }
---
+++ src/dice/DiceEnum.java
@@ -0,0 +1,16 @@
+package src.dice;
+
+public enum DiceEnum {
+    ATTACK,
+    HEALING,
+    STANCE,
+    BLESSING,
+    SHIELD,
+    PINDOWN,
+    GENIE,
+    BESTIAL_POUNCE,
+    DRAIN_LIFE,
+    HEALING_WAVE,
+    JAB,
+    TREAT_WOUNDS;
+}
