9709e0e6a060768838f6674a8a4a1c5e731fee1b
Saathvik Kamma <saathvikkamma@gmail.com>
2026-03-06 09:55:44 -0600

    hmmm

--- src/screens/ConnectionScreen.java
+++ src/screens/ConnectionScreen.java
@@ -5,6 +5,8 @@ import src.networking.GameServer;
 import src.networking.GameSubscriber;
 
 import javax.swing.*;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,12 +29,15 @@ public class ConnectionScreen extends JFrame {
     private final JButton hostBtn = new JButton("Host Host");
     private final JButton joinBtn = new JButton("Join Join");
     private final JButton startGameBtn = new JButton("Start Game");
+    private JTextArea chatLogArea = new JTextArea("", 20, 20);
+    private JTextField messageTextSendArea = new JTextField("", 20);
 
     public ConnectionScreen() {
         super("Dice and Dragons");
-        setSize(560, 267);
+        setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        setLayout(new
+                FlowLayout());
 
 
 
@@ -47,7 +52,22 @@ public class ConnectionScreen extends JFrame {
         add(sentLabel);
         add(receivedLabel);
         add(startGameBtn);
+        dataLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(dataLabel);
+        add(new JLabel("Chat Log:"));
+        chatLogArea.setAlignmentX(SwingConstants.CENTER);
+        chatLogArea.setEditable(false);
+        chatLogArea.setLineWrap(true);
+        for (int i = 0; i < 100; i++){
+            chatLogArea.setText(chatLogArea.getText() + " Hello");
+        }
+        add(chatLogArea);
+
+
+
+
+
+
 
         hostBtn.addActionListener(e -> {
             try {
